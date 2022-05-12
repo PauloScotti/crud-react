da96800b65eaf3cef826576b9ce2045c78303ed4
PauloScotti <paulo.scotti@hotmail.com>
2022-05-12 13:46:16 -0300

    Finalizado

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,10 @@ import Card from './components/card/card';
 
 function App() {
   const [values, setValues] = useState();
-  const [listGames, setListGames] = useState();
+  const [listCard, setListCard] = useState([]);
 
   const handleChangeValues = value => {
-    setValues(prevValue=>({
+    setValues(prevValue => ({
       ...prevValue,
       [value.target.name]: value.target.value,
     }));
@@ -19,58 +19,76 @@ function App() {
       name: values.name,
       cost: values.cost,
       category: values.category,
-    }).then((response) => {
-      console.log(response);
+    }).then(() => {
+      alert("Cadastrado com sucesso");
+      Axios.post("http://localhost:3001/search", {
+        id: values.idgames,
+        name: values.name,
+        cost: values.cost,
+        category: values.category,
+      }).then((response) => {
+        setListCard([
+          ...listCard,
+          {
+            idgames: response.data[0].idgames,
+            name: values.name,
+            cost: values.cost,
+            category: values.category,
+          },
+        ]);
+      });
     });
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     Axios.get("http://localhost:3001/getCards").then((response) => {
-      setListGames(response.data);
+      setListCard(response.data);
     });
   }, []);
 
   return (
-    <div className="app--container">
-      <div className='register--container'>
-        <h1 className='register--title'>Scrim Shop</h1>
-        <input
-          type="text"
-          name="name"
-          placeholder="Nome"
-          className="register--input"
-          onChange={handleChangeValues}
-        />
-        <input
-          type="text"
-          name="cost"
-          placeholder="Preço"
-          className="register--input"
-          onChange={handleChangeValues}
-        />
-        <input
-          type="text"
-          name="category"
-          placeholder="Categoria"
-          className="register--input"
-          onChange={handleChangeValues}
-        />
-        <button className='register--button' onClick={() => handleCLickButton()}>Cadastrar</button>
-      </div>
+    <div className='Container'>
+      <div className="app--container">
+        <div className='register--container'>
+          <h1 className='register--title'>Scrim Shop</h1>
+          <input
+            type="text"
+            name="name"
+            placeholder="Nome"
+            className="register--input"
+            onChange={handleChangeValues}
+          />
+          <input
+            type="text"
+            name="cost"
+            placeholder="Preço"
+            className="register--input"
+            onChange={handleChangeValues}
+          />
+          <input
+            type="text"
+            name="category"
+            placeholder="Categoria"
+            className="register--input"
+            onChange={handleChangeValues}
+          />
+          <button className='register--button' onClick={() => handleCLickButton()}>Cadastrar</button>
+        </div>
 
-      {typeof listGames !== "undefined" && listGames.map((value) => {
-        return (
-          <Card
-            key={value.id}
-            listCard={listGames}
-            setListCard={setListGames}
-            id={value.id}
-            name={value.name}
-            cost={value.cost}
-            category={value.category}
-          ></Card>
-        );
-      })}
+        {listCard.map((value) => {
+          return (
+            <Card
+              listCard={listCard}
+              setListCard={setListCard}
+              key={value.id}
+              id={value.idgames}
+              name={value.name}
+              cost={value.cost}
+              category={value.category}
+            ></Card>
+          );
+        })}
+      </div>
     </div>
   );
 }
